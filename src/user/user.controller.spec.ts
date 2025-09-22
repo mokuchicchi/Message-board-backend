0c0a8371b1e687a03b2cd54926672e8c5966585c
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-09-22 13:16:09 +0900

    Userのテストを行いました

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        {
+          provide: UserService, // UserServiceを差し替える
+          useValue: {
+            getUser: jest.fn().mockReturnValue({}), // getUser関数を差し替える
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be defined', async () => {
+    const controller = new UserController(service); // テスト対象のコントローラ作成
+    await controller.getUser(1, 'xxx-xxx-xxx-xxx'); // getUser関数の呼び出し
+    expect(service.getUser).toHaveBeenCalledTimes(1); // 呼び出し回数の確認
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        {
+          provide: UserService, // UserServiceを差し替える
+          useValue: {
+            getUser: jest.fn().mockReturnValue({}), // getUser関数を差し替える
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);

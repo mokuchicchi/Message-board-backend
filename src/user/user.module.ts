353a0e0749f6ba91fe1956ee959394e7c4b9bd4e
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-09-22 13:15:53 +0900

    Userモジュールを作成しました

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from 'src/entities/user.entity';
+import { Auth } from 'src/entities/auth.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Auth])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createHash } from 'crypto';
+import { Auth } from 'src/entities/auth.entity';
+import { User } from 'src/entities/user.entity';
+import { Equal, MoreThan, Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Auth)
+    private authRepository: Repository<Auth>,
+  ) {}
+
+  async createUser(name: string, email: string, password: string) {
+    const hash = createHash('md5').update(password).digest('hex');
+    const record = {
+      name: name,
+      email: email,
+      hash: hash,
+    };
+    await this.userRepository.save(record);
+  }
+
+  async getUser(id: number, token: string) {
+    // ログイン済みチェック
+    const now = new Date();
+    const auth = await this.authRepository.findOne({
+      where: { token: Equal(token), expire_at: MoreThan(now) },
+    });
+    if (!auth) {
+      throw new ForbiddenException();
+    }
+
+    // ユーザー情報を取得
+    const user = await this.userRepository.findOne({
+      where: { id: Equal(id) },
+    });
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+}

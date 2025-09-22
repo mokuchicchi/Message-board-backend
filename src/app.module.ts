94bf8a3cfe27c6fe51c7f60d6432dc250218e1ef
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-09-22 13:14:02 +0900

    セットアップしました

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -6,7 +7,20 @@ import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'user',
+      password: 'pass',
+      database: 'message_board_db',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UserModule,
+    PostModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

f58f37eab914386dce413f8115bd8940d58c3861
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-09-22 13:15:32 +0900

    Postモジュールを作成しました

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MicroPost } from 'src/entities/micropost.entity';
+import { Auth } from 'src/entities/auth.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MicroPost, Auth])],
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostService],
 })
 export class PostModule {}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Auth } from 'src/entities/auth.entity';
+import { MicroPost } from 'src/entities/micropost.entity';
+import { Equal, MoreThan, Repository } from 'typeorm';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(MicroPost)
+    private readonly microPostsRepository: Repository<MicroPost>,
+    @InjectRepository(Auth)
+    private readonly authRepository: Repository<Auth>,
+  ) {}
+
+  async createPost(message: string, token: string) {
+    // ログイン済みかチェック
+    const now = new Date();
+    const auth = await this.authRepository.findOne({
+      where: { token: Equal(token), expire_at: MoreThan(now) },
+    });
+    if (!auth) {
+      throw new ForbiddenException();
+    }
+
+    const record = {
+      user_id: auth.user_id,
+      content: message,
+    };
+    await this.microPostsRepository.save(record);
+  }
+
+  async getList(token: string, start: number = 0, nr_records: number = 1) {
+    const now = new Date();
+    const auth = await this.authRepository.findOne({
+      where: { token: Equal(token), expire_at: MoreThan(now) },
+    });
+    if (!auth) {
+      throw new ForbiddenException();
+    }
+
+    const qb = await this.microPostsRepository
+      .createQueryBuilder('micro_post')
+      .leftJoinAndSelect('user', 'user', 'user.id=micro_post.user_id')
+      .select([
+        'micro_post.id as id',
+        'user.name as user_name',
+        'micro_post.content as content',
+        'micro_post.created_at as created_at',
+      ])
+      .orderBy('micro_post.created_at', 'DESC')
+      .offset(start)
+      .limit(nr_records);
+
+    type ResultType = {
+      id: number;
+      content: string;
+      user_name: string;
+      created_at: Date;
+    };
+    const records = await qb.getRawMany<ResultType>();
+    console.log(records);
+
+    return records;
+  }
+}

4cbf14ec3951805e46a4a2422e477d3ba0fca08c
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-09-26 15:39:16 +0900

    no message

---
+++ src/migrations 15-34-56-555/1758087729791-initial_schema.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitialSchema1758087729791 implements MigrationInterface {
+  name = 'InitialSchema1758087729791';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "umail" TO "email"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" RENAME COLUMN "email" TO "umail"`,
+    );
+  }
+}
